c2065b971aa30ca3895a14ce28717136dcf2db28
ajo01 <amyjojjm2001@gmail.com>
2021-07-16 21:07:23 -0700

    Restaurant renderOrder() - renderDots ongoing

--- screen/Restaurant.js
+++ screen/Restaurant.js
@@ -11,6 +11,7 @@ import {
 import {COLORS, icons, images, SIZES, FONTS} from '../constants';
 
 const Restaurant = ({route, navigation}) => {
+  const scrollX = new Animated.Value(0);
   const [restaurant, setRestaurant] = React.useState(null);
   const [currentLocation, setCurrentLocation] = React.useState(null);
   const [orderItems, setOrderItems] = React.useState([]);
@@ -168,10 +169,43 @@ const Restaurant = ({route, navigation}) => {
     );
   }
 
+  function renderDots() {
+    const dotPosition = Animated.divide(scrollX, SIZES.width);
+    return (
+      <View style={{height: 30}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: SIZES.padding,
+          }}>
+          {restaurant?.menu.map((item, index) => {
+            const opacity = dotPosition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [0.3, 1, 0.3],
+              extrapolate: 'clamp',
+            });
+            const dotSize = dotPosition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [SIZES.base * 0.8, 10, SIZES.base * 0.8],
+              extrapolate: 'clamp',
+            });
+          })}
+        </View>
+      </View>
+    );
+  }
+
+  function renderOrder() {
+    return <View>{renderDots()}</View>;
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       {renderHeader()}
       {renderFoodInfo()}
+      {renderOrder()}
     </SafeAreaView>
   );
 };
