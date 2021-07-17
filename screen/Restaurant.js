f5136d97d264c9d0a37c104f466655adff6779fe
ajo01 <amyjojjm2001@gmail.com>
2021-07-16 18:35:45 -0700

    Restaurant - states

--- screen/Restaurant.js
+++ screen/Restaurant.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Animated,
+} from 'react-native';
+import {COLORS, icons, images, SIZES, FONTS} from '../constants';
 
-const Restaurant = () => {
+const Restaurant = ({route}) => {
+  const [restaurant, setRestaurant] = React.useState(null);
+  const [currentLocation, setCurrentLocation] = React.useState(null);
+  const [orderItems, setOrderItems] = React.useState([]);
+
+  React.useEffect(() => {
+    let {item, currentLocation} = route.params;
+
+    setRestaurant(item);
+    setCurrentLocation(currentLocation);
+  });
+
   return (
-    <View>
+    <SafeAreaView>
       <Text>Restaurant</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
