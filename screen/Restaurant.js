eae747d9c0cea64f288cb7d0c225936f69804546
ajo01 <amyjojjm2001@gmail.com>
2021-07-16 22:40:47 -0700

    Restaurant renderOrder - editOrder action +

--- screen/Restaurant.js
+++ screen/Restaurant.js
@@ -23,6 +23,29 @@ const Restaurant = ({route, navigation}) => {
     setCurrentLocation(currentLocation);
   });
 
+  function editOrder(action, menuId, price) {
+    let orderList = orderItems.slice();
+    let item = orderList.filter(a => a.menuId == menuId);
+
+    if (action == '+') {
+      if (item.length > 0) {
+        let newQty = item[0].qty + 1;
+        item[0].qty = newQty;
+        item[0].total = item[0].qty * price;
+      } else {
+        const newItem = {
+          menuId: menuId,
+          qty: 1,
+          price: price,
+          total: price,
+        };
+        orderList.push(newItem);
+      }
+
+      setOrderItems(orderList);
+    }
+  }
+
   function renderHeader() {
     return (
       <View style={{flexDirection: 'row'}}>
