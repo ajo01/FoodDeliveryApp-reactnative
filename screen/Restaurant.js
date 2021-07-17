bc626ec388b09e2cef5ccadf30fc8b4b76d8e743
ajo01 <amyjojjm2001@gmail.com>
2021-07-16 18:41:55 -0700

    Restaurant - renderHeader started

--- screen/Restaurant.js
+++ screen/Restaurant.js
@@ -10,7 +10,7 @@ import {
 } from 'react-native';
 import {COLORS, icons, images, SIZES, FONTS} from '../constants';
 
-const Restaurant = ({route}) => {
+const Restaurant = ({route, navigation}) => {
   const [restaurant, setRestaurant] = React.useState(null);
   const [currentLocation, setCurrentLocation] = React.useState(null);
   const [orderItems, setOrderItems] = React.useState([]);
@@ -22,11 +22,33 @@ const Restaurant = ({route}) => {
     setCurrentLocation(currentLocation);
   });
 
+  function renderHeader() {
+    return (
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity
+          style={{
+            width: 50,
+            paddingLeft: SIZES.padding * 2,
+            justifyContent: 'center',
+          }}
+          onPress={() => navigation.goBack()}></TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      {renderHeader()}
       <Text>Restaurant</Text>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.lightGray2,
+  },
+});
 
 export default Restaurant;
