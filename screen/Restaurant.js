4e3413766bc2491b4513b37e7c70639983e11f58
ajo01 <amyjojjm2001@gmail.com>
2021-07-16 21:09:03 -0700

    Restaurant renderOrder() - renderDots showing

--- screen/Restaurant.js
+++ screen/Restaurant.js
@@ -191,6 +191,25 @@ const Restaurant = ({route, navigation}) => {
               outputRange: [SIZES.base * 0.8, 10, SIZES.base * 0.8],
               extrapolate: 'clamp',
             });
+            const dotColor = dotPosition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [COLORS.darkgray, COLORS.primary, COLORS.darkgray],
+              extrapolate: 'clamp',
+            });
+
+            return (
+              <Animated.View
+                key={`dot-${index}`}
+                opacity={opacity}
+                style={{
+                  borderRadius: SIZES.radius,
+                  marginHorizontal: 6,
+                  width: dotSize,
+                  height: dotSize,
+                  backgroundColor: dotColor,
+                }}
+              />
+            );
           })}
         </View>
       </View>
