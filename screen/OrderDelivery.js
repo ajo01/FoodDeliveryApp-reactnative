0c32c7cde7415f2accad930507e7d4dfd30c4c60
ajo01 <amyjojjm2001@gmail.com>
2021-07-25 17:12:22 -0700

    OrderDelivery - states and useeffect for initial states

--- screen/OrderDelivery.js
+++ screen/OrderDelivery.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import {View, Text, SafeAreaView, Image, TouchableOpacity} from 'react-native';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import {COLORS, FONTS, icons, SIZES, GOOGLE_API_KEY} from '../constants';
 
-const OrderDelivery = () => {
+const OrderDelivery = ({route, navigation}) => {
+  const [restaurant, setRestaurant] = React.useState(null);
+  const [streetName, setStreetName] = React.useState('');
+  const [fromLocation, setFromLocation] = React.useState(null);
+  const [toLocation, setToLocation] = React.useState(null);
+  const [region, setRegion] = React.useState(null);
+
+  React.useEffect(() => {
+    let {restaurant, currentLocation} = route.params;
+
+    let fromLoc = currentLocation.gps;
+    let toLoc = restaurant.location;
+    let street = currentLocation.streetName;
+
+    let mapRegion = {
+      latitude: (fromLoc.latitude + toLoc.latitude) / 2,
+      longitude: (fromLoc.longitude + toLoc.longitude) / 2,
+      latitudeDelta: Math.abs(fromLoc.latitude - toLoc.latitude) * 2,
+      longitudeDelta: Math.abs(fromLoc.longitude - toLoc.longitude) * 2,
+    };
+
+    setRestaurant(restaurant);
+    setStreetName(street);
+    setFromLocation(fromLoc);
+    setToLocation(toLoc);
+    setRegion(mapRegion);
+  }, []);
+
   function renderMap() {
     return (
       <View style={{flex: 1}}>
